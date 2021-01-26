5e92538fd7c60252ba2b895c1e72c33f218b9e43
Monssaf Toukal <tmonssaf@gmail.com>
2021-01-26 12:55:47 -0500

    test solver_bmark

--- benchmark/solver_benchmark.jl
+++ benchmark/solver_benchmark.jl
@@ -21,7 +21,14 @@ Plots.default(; common_plot_args...)
 # perform benchmarks
 results = PkgBenchmark.benchmarkpkg("Krylov", script="benchmark/cg_bmark.jl")
 
+println("type : ", typeof(results), "eltype: ", eltype(results))
+println("results: ", results)
+die()
 # process benchmark results and post gist
 p = profile_solvers(results)
 posted_gist = to_gist(results, p)
 println(posted_gist.html_url)
+
+# open("gist.json", "w") do f
+#   JSON.print(f, posted_gist)
+# end
