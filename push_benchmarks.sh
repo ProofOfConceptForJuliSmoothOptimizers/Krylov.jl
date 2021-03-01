dbb7d63995520277f66bfeed2cc2466655bee12f
JSOBot <69814847+JSOBot@users.noreply.github.com>
2021-03-01 13:04:36 -0500

    adding/updating file: push_benchmarks.sh

--- push_benchmarks.sh
+++ push_benchmarks.sh
@@ -2,7 +2,9 @@
 
 julia benchmark/send_comment_to_pr.jl -o $org -r $repo -p $pullrequest -c '**Starting benchmarks!**'  
 
-if julia benchmark/$1 ; then
+bmark_command = $(julia benchmark/$1)
+
+if [ "$?" -eq "0" ] ; then
     julia benchmark/send_comment_to_pr.jl -o $org -r $repo -p $pullrequest -g
 else
     julia benchmark/send_comment_to_pr.jl -o $org -r $repo -p $pullrequest -c "**An error has occured while running the benchmark script: $1** "
