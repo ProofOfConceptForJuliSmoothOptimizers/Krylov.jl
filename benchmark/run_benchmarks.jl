f17f4018755a8033daa0dbad07559f80236abed0
Monssaf Toukal <tmonssaf@gmail.com>
2021-03-08 11:54:35 -0500

    still debugging

--- benchmark/run_benchmarks.jl
+++ benchmark/run_benchmarks.jl
@@ -49,12 +49,10 @@ files_dict = Dict{String, Any}()
 file_num = 1
 for k ∈ keys(judgement_stats)
   global file_num
-  if k == :baseline
-    [println("Keys ", i, " :", key) for (i,key) in enumerate(keys(judgement_stats))]
-    println("commit stats: ", keys(commit_stats))
-    println("master stats: ", keys(master_stats))
-    die()
-  end
+  [println("Keys ", i, " :", key) for (i,key) in enumerate(keys(judgement_stats))]
+  println("commit stats: ", keys(commit_stats))
+  println("master stats: ", keys(master_stats))
+  die()
   k_stats = Dict{Symbol,DataFrame}(:commit => commit_stats[k],
                                    :master => master_stats[k])
   save_stats(k_stats, "ldl_$(bmarkname)_vs_master_$(k).jld2", force=true)
