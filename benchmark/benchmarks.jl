ed92b046153315eb375221f306a4dfc4594a5390
Monssaf Toukal <tmonssaf@gmail.com>
2021-03-08 15:07:07 -0500

    changing suite again to debug

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -17,31 +17,30 @@ fetch_ssmc(ufl_posdef, format="MM")
 
 const SUITE = BenchmarkGroup()
 
-SUITE["CG"] = BenchmarkGroup() # BenchmarkGroup(["CG", "SPD"])
+# SUITE["CG"] = BenchmarkGroup() # BenchmarkGroup(["CG", "SPD"])
 
-for N in [32, 64, 128]
-  # SUITE["CG"]["DivGrad N=$N"] = BenchmarkGroup()
-  A = get_div_grad(N, N, N)
-  n = size(A, 1)
-  b = ones(n)
-  op = PreallocatedLinearOperator(A)
-  M = opEye()
-  rtol = 1.0e-6
-  SUITE["CG"]["DivGrad N=$N"] = @benchmarkable cg($op, $b, M=$M, atol=0.0, rtol=$rtol, itmax=$n)
-end
-
-# SUITE["CG"]["UFL"] = BenchmarkGroup()
-# for matrix in ufl_posdef
-#   name = matrix.name
-#   A = MatrixMarket.mmread(joinpath(matrix_path(matrix, format="MM"), "$(name).mtx"))
-#   if eltype(A) <: Integer
-#     A = convert(SparseMatrixCSC{Float64,Int}, A)
-#   end
+# for N in [32, 64, 128]
+#   SUITE["CG"]["DivGrad N=$N"] = BenchmarkGroup()
+#   A = get_div_grad(N, N, N)
 #   n = size(A, 1)
-#   b = ones(eltype(A), n)
+#   b = ones(n)
 #   op = PreallocatedLinearOperator(A)
 #   M = opEye()
 #   rtol = 1.0e-6
-#   SUITE["CG"]["UFL"][name] = BenchmarkGroup()
-#   SUITE["CG"]["UFL"][name]["Krylov"] = @benchmarkable cg($op, $b, M=$M, atol=0.0, rtol=$rtol, itmax=$n)
+#   SUITE["CG"]["DivGrad N=$N"] = @benchmarkable cg($op, $b, M=$M, atol=0.0, rtol=$rtol, itmax=$n)
 # end
+
+SUITE["UFL"] = BenchmarkGroup()
+for matrix in ufl_posdef
+  name = matrix.name
+  A = MatrixMarket.mmread(joinpath(matrix_path(matrix, format="MM"), "$(name).mtx"))
+  if eltype(A) <: Integer
+    A = convert(SparseMatrixCSC{Float64,Int}, A)
+  end
+  n = size(A, 1)
+  b = ones(eltype(A), n)
+  op = PreallocatedLinearOperator(A)
+  M = opEye()
+  rtol = 1.0e-6
+  SUITE["UFL"][name] = @benchmarkable cg($op, $b, M=$M, atol=0.0, rtol=$rtol, itmax=$n)
+end
